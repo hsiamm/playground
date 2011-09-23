e63d284a0d8e471d781deacbe7892c48797615aa
ASCC Developer <info@austinstone.org>
2011-09-23 16:21:06 -0500

    commiting changes on dev to resolve conflict

---
+++ design/first.php
@@ -0,0 +1,39 @@
+<?php include("includes/header.php"); ?>
+
+<body id="about">
+	
+	<?php include("includes/top.php"); ?>
+	
+	<div class="container">
+	
+		<div class="bump">&nbsp;</div>		
+		<?php include("includes/menu.php"); ?>
+	
+	</div><!--/container-->	
+	
+	<div style="clear:both;"></div>
+
+	<!--/\/\starting line, go!/\/\-->	
+	<div class="container">
+		<div class="onecol">
+			<h1>First Tuesday</h1>
+		</div>	
+		
+		<div style="clear:both"></div>
+		<div class="bump">&nbsp;</div>
+		
+		<div class="twocol">
+			<h4 class="black"><strong>October 4  |  7pm  |  St. John Campus</strong></h4>
+			<p>Join us Tuesday, October 4, at 7pm for our First Tuesday prayer service. Each month we spend time together as a church body praying for God to do things in our lives and in the world around us greater than what we can do on our own. We‘ll be meeting at the new St John Campus at 7pm. The map below shows the location of the new facility.</p>
+		</div>
+		
+		<div class="fivecol_4">
+		<iframe width="738" height="365" frameborder="0" scrolling="no" marginheight="0" style="border: 5px solid #fff;"  marginwidth="0" src="http://maps.google.com/maps/ms?msa=0&amp;msid=211619950205743684538.000465cb9adf20f58d81e&amp;ie=UTF8&amp;vpsrc=0&amp;source=embed&amp;t=m&amp;f=d&amp;daddr=St.+John+Campus+%4030.334202,-97.706646&amp;ll=30.366951,-97.706566&amp;spn=0.10812,0.253029&amp;z=12&amp;iwloc=0004ac605d0f0e22f06fe&amp;output=embed"></iframe>
+		</div>
+		
+	</div><!--/container-->
+		
+	<?php include("includes/footer.php"); ?> 
+	
+</body> 
+</html>
