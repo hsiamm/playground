ddd8e354781ac17ca80d05dcb82cb4b8a722feb2
ASCC Developer <communications@austinstone.org>
2011-12-14 14:48:25 -0600

    added home_images/ to .gitignore file

---
+++ logs/error.php
@@ -0,0 +1,6 @@
+#<?php die('Direct Access To Log Files Not Permitted'); ?>
+#Version: 1.0
+#Date: 2011-12-14 20:42:11
+#Fields: date	time	level	c-ip	status	comment
+#Software: Joomla! 1.6.5 Stable [ Onward ] 11-Jul-2011 23:00 GMT
+2011-12-14	20:42:11	-	24.173.10.194	Joomla FAILURE: 	User does not exist
