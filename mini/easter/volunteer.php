9520f61bdc8a628aa488b63cc8a945f9743946b7
ASCC Developer <communications@austinstone.org>
2012-03-20 10:31:56 -0500

    volunteer removal

--- mini/easter/volunteer.php
+++ mini/easter/volunteer.php
@@ -8,18 +8,21 @@
  	<?php include("includes/menu.php"); ?>
 		
 		<div class="inside_content">
-			<div class="onecol">
+			<!--<div class="onecol">-->
 		
-				<!--<h1>Thank You</h1>
-				<div class="col2_a">
-				<p>Praise God for filling our volunteer needs for this upcoming Easter service! We are so excited to see the church rally together to serve others on Easter Sunday. If you have any further questions about volunteering, please <a href="mailto:janet@ministries.austinstone.org">email us</a>.</p>
+				<div class="onecol">
+				<h1>Thank You</h1>
+				</div>
+				
+				<div class="twocol">
+				<p>Praise God for filling our volunteer needs for this upcoming Easter service! We are so excited to see the church rally together to serve others on Easter Sunday. If you have any further questions about volunteering, please <a href="mailto:jill.lejcar@austinstone.org">email us</a>.</p>
 				</div> 
 				
-				<div class="col2_b">
-				<img src="/images/easter/stairs.jpg" alt="Frank Erwin Center"/>
-				</div>-->
+				<div class="twocol">
+				<img src="/easter/images/stairs.jpg" alt="Frank Erwin Center"/>
+				</div>
 			
-				<script type="text/javascript">var host = (("https:" == document.location.protocol) ? "https://secure." : "http://");document.write(unescape("%3Cscript src='" + host + "wufoo.com/scripts/embed/form.js' type='text/javascript'%3E%3C/script%3E"));</script>
+				<!--<script type="text/javascript">var host = (("https:" == document.location.protocol) ? "https://secure." : "http://");document.write(unescape("%3Cscript src='" + host + "wufoo.com/scripts/embed/form.js' type='text/javascript'%3E%3C/script%3E"));</script>
 				
 				<script type="text/javascript">
 				var w6w2p7 = new WufooForm();
@@ -32,7 +35,7 @@
 				'ssl':true});
 				w6w2p7.display();
 				</script>
-			</div><!--/onecol-->
+			</div>--><!--/onecol-->
 		<div style="clear:both;"></div><!--/clear-->
 	
 		</div><!--/inside_content-->
