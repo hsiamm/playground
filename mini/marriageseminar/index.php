0ccee70a5e22a4959f9844a5dc327f94a6d7a526
ASCC Developer <communications@austinstone.org>
2012-04-04 12:18:14 -0500

    Marriage Seminar launch

---
+++ mini/marriageseminar/index.php
@@ -0,0 +1,68 @@
+<?php include("includes/header.php"); ?>
+
+<body id="home"> 
+		
+		<?php include("includes/top.php"); ?>
+		
+		<div class="col2">
+			<p>Join us June 1-2 as speaker Paul David Tripp will teach us practical insights for the everyday issues and situations of a real marriage. You will learn learn the practical steps that will help couples know how to get from where they are, to where God wants them to be.</p>
+
+			<p>In this conference, Paul Tripp will explore the experience of realizing that your marriage hasn't escaped the reality of sin or the brokenness of the world. It's what you do next that will make all the difference in the world in the character and quality of your marriage.  The surrounding culture would lead us to believe that a marriage of unity, love, and understanding is rooted in romance. Scripture would rather say that a good marriage is the result of worship. Does this confuse you? Then, this marriage conference is for you.</p>
+			
+			<p>This conference is for individuals, engaged couples, and married couples at any stage of marriage.</p>
+		</div>
+		
+		<div class="scheduletop" style="margin-top:14px;">
+			<h3 style="color:#fff;font-weight:600;">Schedule of Events</h3>
+		</div>
+		
+		<div class="schedule" style="background-color:#f9f9f9;">
+			<h1 style="color:#193D4F;margin-top:20px;">June 01</h1>
+			<p class="time" style="margin:0;">6:30pm&ndash;9:30pm</p><br>
+			<h1 style="color:#193D4F;">June 02</h1>
+			<p class="time" style="margin:0;">8:00am&ndash;12:00pm</p>
+		</div>
+		
+		<div class="rule"></div>
+		<div style="clear:both;">&nbsp;</div>
+		<div class="col2">
+			<div id="map">
+				<div class="slides_container">
+					<img src="/marriageseminar/images/stjohn.jpg" alt="St. John Campus"/>
+					<iframe width="466" height="311" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=500+E+Street+Johns+Avenue,+Austin,+TX+St.+John+Campus+The+Austin+Stone&amp;aq=&amp;sll=30.327554,-97.693614&amp;sspn=0.007529,0.012048&amp;g=500+E+Street+Johns+Avenue,+Austin,+TX+St.+John+Campus&amp;ie=UTF8&amp;hq=500+E+Street+Johns+Avenue,+Austin,+TX+St.+John+Campus+The+Austin+Stone&amp;hnear=&amp;radius=15000&amp;t=m&amp;cid=11301589955820909374&amp;ll=30.390053,-97.775917&amp;spn=0.092103,0.159645&amp;z=12&amp;output=embed"></iframe>
+				</div>
+				<ul class="pagination">
+					<li><a href="#"><h2 class="slide-button">St. John Campus</h2></a></li>
+					<li><a href="#"><h2 class="slide-button">View Google Map</h2></a></li>
+				</ul>
+			</div>
+		</div><!--/col2-->
+		
+		<div class="colwhere">
+			<h1>Where</h1>
+			<p>The seminar will be held at our St. John Campus in the For the City Center. The St. John Campus is located at 500 E St Johns Ave.</p>
+		</div>
+		
+		<div style="clear:both;"></div>
+		<div class="bump">&nbsp;</div>
+
+		<div class="rule"></div>
+		<div style="clear:both;"></div>
+		
+		<div style="margin-top:10px"><h1>Cost and Details</h1></div>
+		
+		<div class="col1 details">
+			<h1 class="price">$20</h1>
+			<p style="text-align:center;font-size:12px;font-weight:600;">Per person until May 20</p>
+		</div>
+		
+		<div class="col1 details">
+			<h1 class="price">$30</h1>
+			<p style="text-align:center;font-size:12px;font-weight:600;">Per person starting May 21</p>
+		</div>
+		
+		<div class="col1b details">
+			<h2 style="text-align:center;color:#EA643B;line-height:24px;height:63px;margin-top:5px;">Limited Childcare Available</h2><p style="text-align:center;font-size:12px;font-weight:600;">$25 per child for the weekend</p>
+		</div>
+		
+		<?php include("includes/footer.php"); ?>
---
+++ mini/marriageseminar/leaders.php
@@ -0,0 +1,39 @@
+<?php include("includes/header.php"); ?>
+
+<body id="leaders"> 
+
+		<?php include("includes/top.php"); ?>
+		
+		<div class="col1">
+			<img src="/marriageseminar/images/paul.png" alt="Paul Tripp"/>
+			<h2 style="text-align:center;font-size:18px;margin-top:16px;">Speaker:</h2>
+			<p style="text-align:center;color:#99C4C2;margin-top:0px;">Paul Tripp</p>
+		</div>
+		<div class="col2">
+			<p>Paul Tripp is the president of <a href="http://www.paultrippministries.com/home">Paul Tripp Ministries</a>, a nonprofit organization, whose mission statement is &ldquo;Connecting the transforming power of Jesus Christ to everyday life.&rdquo; This mission leads Paul to weekly speaking engagements around the world.  In addition to being a gifted communicator and sought after conference speaker with Paul Tripp Ministries, Paul is on the pastoral staff at Tenth Presbyterian Church in Philadelphia, Pennsylvania where he preaches on Sunday evenings and leads the Ministry to Center City.</p>
+			<p>Paul is also Professor of Pastoral Life and Care at Redeemer Seminary in Dallas, Texas; he is the Director of the Center for Pastoral Life and Care in Fort Worth, Texas, and has taught at respected institutions worldwide. As an author, Paul has written ten books on Christian Living that are read and distributed internationally. He has been married for many years to Luella and they have four grown children.</p>
+		</div>
+		
+		<div style="clear:both;height:30px;">&nbsp;</div>
+		
+		<div class="col1">
+			<img src="/marriageseminar/images/jc.png" alt="Justn Cofield"/>
+			<h2 style="text-align:center;font-size:18px;margin-top:16px;">Worship Leader:</h2>
+			<p style="text-align:center;color:#99C4C2;margin-top:0px;">Justin Cofield</p>
+		</div>
+				 	
+	<!--	<div class="col1" style="margin:0 30px 0 0;">
+			<img src="/images/parenting_seminar/andy.png" alt="Paul Tripp"/><br>
+			<h2 style="text-align:center;font-size: 18px;">Worship Leader:</h2>
+			<h3 style="text-align:center;color:#231F20;text-transform:none;font-size:14px;">Andy Melvin</h3>
+		</div>-->
+		
+		<div class="col2">
+			<p>Justin Cofield has spent the past 12 years leading people in worship around the country. His desire to invest in people has permeated his life both on and off the stage. He defines himself as: &ldquo;a follower of Jesus, a husband, father, and a lover of the forgotten.&rdquo; He and his wife are passionate about the redemption of all people, including their neighborhood on the East side of Austin, TX.</p>
+			<p>As a worship leader for <a href="/">The Austin Stone Community Church</a>, he continues to pursue his desire for leading others in response to who God is and what He has done both in song and in life. Justin and his wife Jana have one son, and they are currently pursuing the adoption of a daughter from China.</p>
+		</div>	
+
+		<div style="clear:both;"></div>
+		<div class="bump">&nbsp;</div>
+				
+		<?php include("includes/footer.php"); ?>
